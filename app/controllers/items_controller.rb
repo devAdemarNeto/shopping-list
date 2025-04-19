8a70a0f387f1b3a566d2a04f9021e0c451db4f23
Ademar Neto <ademarnetodev@gmail.com>
2025-04-18 23:43:13 -0300

    Implementa ItemsController com todas ações e views

---
+++ app/controllers/items_controller.rb
@@ -0,0 +1,59 @@
+class ItemsController < ApplicationController
+  before_action :set_item, only: [:update, :destroy, :toggle]
+  def index
+    @items = Item.all.order(created_at: :desc)
+    @new_item = Item.new # Para o Formulário de criação
+  end
+
+  # Post / items
+  def create
+    @item = Item.new(item_params)
+
+    respond_to do |format|
+      if @item.save
+        format.html { redirect_to root_path, notice: 'Item adicionado com sucesso' }
+        format.turbo_stream #Procura por create.turbo_stream.erb
+      else
+        format.html { render :index, status: : unprocessable_entity }
+        format.turbo_stream { render turbo_stream: turbo_stream.replace('new_item_form', partial: 'items/form', locals: { item: @item }) }
+      end
+    end
+  end
+
+  #PATCH/PUT /item/:id
+  def update
+    if @item.update(item_params)
+      redirect_to root_path, notice: 'Item atualizado com sucesso'
+    else
+      render :index, status: unprocessable_entity
+    end
+  end
+
+  def destroy
+    @item.destroy
+    respond_to do |format|
+      format.html { redirect_to root_path, notice: 'Item removido com sucesso' }
+      format.turbo_stream # Procura por destroy.turbo_stream.erb
+    end
+  end
+
+  
+  # PATCH /items/:id/toggle
+  def toggle
+    @item.update(purchased: !@item.purchased)
+    format.html { redirect_to root_path }
+    format.turbo_stream # Procura por toggle.turbo_stream.erb
+  end
+end
+
+
+private
+
+  def set_item
+    @item = Item.find(params[ :id])
+  end
+
+  def item_params
+    params.require(:item).permit(:name, :quantity)
+  end
+end
